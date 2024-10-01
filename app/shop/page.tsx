2572beb085c67a759f844d6df7c75cf86553c8f2
gkwojtak <grzegorz.wojtak@gmail.com>
2024-10-01 17:00:46 +0200

    chore: t&c

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -10,15 +10,15 @@ export default function Page() {
 
             <div className={'pt-24 px-6 max-w-xl text-center mx-auto mb-24'}>
                 <div className={'space-y-12 '}>
-                    <h1 className={'text-4xl font-bold text-slate-700  -tracking-wider'}>We're <span
+                    <h1 className={'text-4xl font-bold text-slate-700  -tracking-wider'}>We&apos;re <span
                         className={'text-teal-500'}>Launching</span> Soon</h1>
                     <div className={'space-y-6 text-xl leading-8'}>
-                        <p className={'text-slate-600'}>We're excited to announce that our shop will be opening soon, offering you the chance to purchase exclusive, personalized products directly from us. While the official launch is just around the corner, we understand that you may have questions or want to explore your options now. That's why we're here to help!</p>
+                        <p className={'text-slate-600'}>We&apos;re excited to announce that our shop will be opening soon, offering you the chance to purchase exclusive, personalized products directly from us. While the official launch is just around the corner, we understand that you may have questions or want to explore your options now. That&apos;s why we&apos;re here to help!</p>
                         <p className={'text-slate-600'}>
-                            Feel free to reach out to us directly for detailed information about our products, pricing, and how the process works. Whether you're curious about customization options or simply want to be among the first to know when we go live, we’re happy to assist you.
+                            Feel free to reach out to us directly for detailed information about our products, pricing, and how the process works. Whether you&apos;re curious about customization options or simply want to be among the first to know when we go live, we’re happy to assist you.
                         </p>
                         <p className={'text-slate-600'}>
-                            By contacting us now, you'll get a head start on understanding everything we offer before the official launch. Stay tuned—exciting new products are coming your way, and we can't wait to share them with you!
+                            By contacting us now, you&apos;ll get a head start on understanding everything we offer before the official launch. Stay tuned—exciting new products are coming your way, and we can&apos;t wait to share them with you!
                         </p>
                     </div>
                     <div className={'flex gap-4 justify-center flex-wrap'}>
